8211d5056cd5032d912baacc5f97d87b94f4d6f0
José Ramírez Dure <132848165+joseram03@users.noreply.github.com>
2025-03-26 11:04:41 -0300

    Proyecto Inicial

---
+++ src/main/java/com/example/POS_MJ/PosMjApplication.java
@@ -0,0 +1,13 @@
+package com.example.POS_MJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosMjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosMjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/POS_MJ/PosMjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.POS_MJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosMjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
