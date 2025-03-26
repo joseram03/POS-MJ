04f6591e06b9396eea4b2429115a7935d59af127
José Ramírez Dure <132848165+joseram03@users.noreply.github.com>
2025-03-26 11:45:59 -0300

    Update

---
+++ POS-MJ-BACK/src/main/java/com/example/POS_MJ_BACK/PosMjBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.POS_MJ_BACK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosMjBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosMjBackApplication.class, args);
+	}
+
+}
---
+++ POS-MJ-BACK/src/test/java/com/example/POS_MJ_BACK/PosMjBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.POS_MJ_BACK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosMjBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
