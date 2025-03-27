62e21abe15b14bac385c9f1df8f2b87030acd954
José Ramírez Dure <132848165+joseram03@users.noreply.github.com>
2025-03-27 13:44:29 -0300

    Minor updates

--- POS-MJ-BACK/src/main/java/com/example/POS_MJ_BACK/PosMjBackApplication.java
+++ POS-MJ-BACK/src/main/java/com/example/POS_MJ_BACK/PosMjBackApplication.java
@@ -2,8 +2,10 @@ package com.example.POS_MJ_BACK;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.POS_MJ_BACK.config")
 public class PosMjBackApplication {
 
 	public static void main(String[] args) {
