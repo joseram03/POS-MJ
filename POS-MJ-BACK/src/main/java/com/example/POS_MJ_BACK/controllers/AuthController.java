4e4d9f30da7c6b50b90448f1d4c54d0e4a2b94d5
José Ramírez Dure <132848165+joseram03@users.noreply.github.com>
2025-03-28 11:31:04 -0300

    AuthController update
    Actualizacion parcial y completo agregados

--- POS-MJ-BACK/src/main/java/com/example/POS_MJ_BACK/controllers/AuthController.java
+++ POS-MJ-BACK/src/main/java/com/example/POS_MJ_BACK/controllers/AuthController.java
@@ -7,6 +7,8 @@ import com.example.POS_MJ_BACK.models.Rol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.webauthn.api.PublicKeyCose;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,10 +25,11 @@ public class AuthController {
     private RolService rolService;
 
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
 
     // Registro de usuario
-    @PostMapping("/registro")
+    // Es requisito: nombre, usuario, correo, contrasenha
+    @PostMapping("/registro") //Tested
     public ResponseEntity<String> registrarUsuario(@RequestBody Usuario usuario) {
         try {
             // Asignar un rol 'vendedor' por defecto
@@ -47,7 +50,8 @@ public class AuthController {
     }
 
     // Inicio de sesión (Ejemplo simple sin seguridad completa)
-    @PostMapping("/login")
+    // Solo valida usuario y contrasenha
+    @PostMapping("/login") //Tested
     public ResponseEntity<String> login(@RequestBody Usuario usuario) {
         try {
             Usuario usuarioExistente = usuarioService.obtenerTodosLosUsuarios().stream()
@@ -67,22 +71,41 @@ public class AuthController {
     }
 
     // Obtener todos los usuarios (solo para administradores)
-    @GetMapping("/usuarios")
+    @GetMapping("/usuarios") //Tested
     public List<Usuario> obtenerTodosLosUsuarios() {
         return usuarioService.obtenerTodosLosUsuarios();
     }
 
-    // Obtener usuario por ID (solo para administradores)
-    @GetMapping("/usuarios/{id}")
+    // Obtener usuario por ID (solo para administradores implementar)
+    @GetMapping("/usuarios/{id}") //Tested
     public Usuario obtenerUsuarioPorId(@PathVariable Long id) {
         return usuarioService.obtenerUsuarioPorId(id);
     }
 
-    // Actualizar usuario
-    @PutMapping("/usuarios/{id}")
-    public ResponseEntity<String> actualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuario) {
+    // Actualizar usuario completo
+    // Requisito: nombre, usuario, correo, contrasenha, rol (si no especifica es "vendedor")
+    @PutMapping("/usuarios/{id}") //Tested
+    public ResponseEntity<String> actualizarUsuarioCompleto(@PathVariable Long id, @RequestBody Usuario usuario) {
         try {
-            usuarioService.actualizarUsuario(id, usuario);
+            // Como el Rol podemos optener parcialmente, recuperamos completo
+            usuario.setRol(rolService.obtenerRol(usuario.getRol()));
+            usuarioService.actualizarUsuarioCompleto(id, usuario);
+            return ResponseEntity.ok("Usuario actualizado exitosamente.");
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body("Error al actualizar el usuario: " + e.getMessage());
+        }
+    }
+
+    // Actualizar usuario Parcial
+    // Requisito: solo atributos a cambiar, el rol puede ser parcial
+    @PatchMapping("/usuarios/{id}") //Tested
+    public ResponseEntity<String> actualizarUsuarioParcial(@PathVariable Long id, @RequestBody Usuario usuario) {
+        try {
+            // Recuperamos el Rol si es que mandaron
+            if(usuario.getRol() != null){
+                usuario.setRol(rolService.obtenerRol(usuario.getRol()));
+            }
+            usuarioService.actualizarUsuarioParcial(id, usuario);
             return ResponseEntity.ok("Usuario actualizado exitosamente.");
         } catch (Exception e) {
             return ResponseEntity.status(400).body("Error al actualizar el usuario: " + e.getMessage());
@@ -90,7 +113,7 @@ public class AuthController {
     }
 
     // Eliminar usuario
-    @DeleteMapping("/usuarios/{id}")
+    @DeleteMapping("/usuarios/{id}") //Tested
     public ResponseEntity<String> eliminarUsuario(@PathVariable Long id) {
         try {
             usuarioService.eliminarUsuario(id);
