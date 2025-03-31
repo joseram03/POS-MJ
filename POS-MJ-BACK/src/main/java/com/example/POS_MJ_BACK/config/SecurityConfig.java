aa7f61d2f30366a36281718c240d813838014868
José Ramírez Dure <132848165+joseram03@users.noreply.github.com>
2025-03-31 13:10:40 -0300

    Configuracion de seguridad para JWT

--- POS-MJ-BACK/src/main/java/com/example/POS_MJ_BACK/config/SecurityConfig.java
+++ POS-MJ-BACK/src/main/java/com/example/POS_MJ_BACK/config/SecurityConfig.java
@@ -1,24 +1,60 @@
 package com.example.POS_MJ_BACK.config;
 
+import com.example.POS_MJ_BACK.security.JwtAuthenticationFilter;
+import com.example.POS_MJ_BACK.security.JwtAuthorizationFilter;
+import com.example.POS_MJ_BACK.security.JwtTokenProvider;
+import com.example.POS_MJ_BACK.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())  // Desactiva CSRF para APIs REST
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()  // Permite todo sin autenticación
-            );
-        
-        return http.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenProvider);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login");
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/auth/login", "/auth/registro").permitAll();
+                    // Esto es un ej si queremos restringir toda una pagina /pag a un tipo de rol
+                    //auth.requestMatchers("/ventas/**").hasAnyAuthority("ROLE_VENDEDOR", "ROLE_ADMINISTRADOR");
+                    auth.anyRequest().authenticated();
+                })
+                .addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(new JwtAuthorizationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
     }
 
     @Bean
