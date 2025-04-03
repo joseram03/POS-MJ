7c9f8e313e46b8df309e56b88d37ed6989d35be0
José Ramírez Dure <132848165+joseram03@users.noreply.github.com>
2025-04-03 15:30:36 -0300

    Fron config

--- pos-system/src/app/app.component.ts
+++ pos-system/src/app/app.component.ts
@@ -12,7 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     CommonModule,
     RouterModule,
-    HttpClientModule, // Asegúrate de que esto está presente
+    HttpClientModule,
     NavbarComponent,
     ProductListComponent,
     CartSummaryComponent
