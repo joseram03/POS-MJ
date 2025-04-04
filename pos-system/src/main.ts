aab0a79491cd9d2a47fc55a4d16bac597da9a0c0
José Ramírez Dure <132848165+joseram03@users.noreply.github.com>
2025-04-04 11:30:08 -0300

    Themas ya solucionado

--- pos-system/src/main.ts
+++ pos-system/src/main.ts
@@ -2,29 +2,24 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
+import Aura from '@primeng/themes/aura';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes'; // If using routing
-
+import { routes } from './app/app.routes'; // Si usas rutas
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
 import { MessageService } from 'primeng/api';
-
-
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideAnimations(), // Required for PrimeNG animations
-    provideHttpClient(), // Required for HTTP calls (if using HttpClient)
-    provideRouter(routes), // Only if using Angular Router
-    // Add other global providers here (e.g., services)
+    provideAnimations(),       // Necesario para PrimeNG
+    provideHttpClient(),       // Para peticiones HTTP
+    provideRouter(routes),     // Si usas Angular Router
+    MessageService,
     provideAnimationsAsync(),
-        providePrimeNG({
+        providePrimeNG({ 
             theme: {
                 preset: Aura
             }
-        }),
-    MessageService
+        })
   ]
 }).catch(err => console.error(err));
