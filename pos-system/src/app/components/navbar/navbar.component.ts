a2ef6262aee2592177ffc8add3af7ecdc943f7b6
José Ramírez Dure <132848165+joseram03@users.noreply.github.com>
2025-04-04 11:29:47 -0300

    Logout implementation

--- pos-system/src/app/components/navbar/navbar.component.ts
+++ pos-system/src/app/components/navbar/navbar.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MenuItem } from 'primeng/api';
 import { TabMenuModule } from 'primeng/tabmenu';
 import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, TabMenuModule, AvatarModule],
+  imports: [CommonModule, TabMenuModule, AvatarModule, ButtonModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
@@ -15,10 +17,19 @@ export class NavbarComponent implements OnInit {
   items: MenuItem[] = [];
   activeItem: MenuItem = {};
 
+  constructor(private router: Router) {}
+
   ngOnInit() {
     this.items = [
       { label: 'Productos', icon: 'pi pi-box' }
     ];
     this.activeItem = this.items[0];
+  }
+
+  logout(): void {
+    // Remueve el token de sessionStorage
+    sessionStorage.removeItem('token');
+    // Redirige a la página de login (ajusta la ruta según tu aplicación)
+    this.router.navigate(['login']);
   }
 }
